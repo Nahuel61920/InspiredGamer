46345ca4b68391715b4fcc20b866707d1976fe4e
Nahuel619 <estebancarrizo619@gmail.com>
2022-02-05 04:58:23 -0300

    Agregados juegos destacados

--- js/main.js
+++ js/main.js
@@ -24,3 +24,32 @@ window.onscroll = () =>{
     menuBtn.classList.remove("fa-times");
     navbar.classList.remove('active');
 }
+
+
+var swiper = new Swiper(".destacados-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+        slidesPerView: 1,
+        },
+        450: {
+        slidesPerView: 2,
+        },
+        768: {
+        slidesPerView: 3,
+        },
+        1024: {
+        slidesPerView: 4,
+        },
+    },
+});
