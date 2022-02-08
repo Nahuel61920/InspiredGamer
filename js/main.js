5dd4fa96acf5599cfa4e8bb99314de8d8c0cd216
Nahuel619 <estebancarrizo619@gmail.com>
2022-02-08 03:51:45 -0300

    Agregando juegos nuevos

--- js/main.js
+++ js/main.js
@@ -42,10 +42,35 @@ var swiper = new Swiper(".destacados-slider", {
         0: {
         slidesPerView: 1,
         },
+        768: {
+        slidesPerView: 2,
+        },
+        1024: {
+        slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".new-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+        slidesPerView: 1,
+        },
         450: {
         slidesPerView: 2,
         },
-        768: {
+        892: {
         slidesPerView: 3,
         },
         1024: {
