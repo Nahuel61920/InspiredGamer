0296ad657a0dce96345bbb7bec7d68c0f2903c17
Nahuel61920 <estebancarrizo619@gmail.com>
2022-12-02 23:59:08 -0300

    New Address

--- client/pages/account.tsx
+++ client/pages/account.tsx
@@ -8,6 +8,7 @@ import { getMeApi } from '../pages/api/user';
 import ChangeNameForm from '../components/Account/ChangeNameForm/ChangeNameForm';
 import ChangeEmailForm from '../components/Account/ChangeEmailForm/ChangeEmailForm';
 import ChangePasswordForm from '../components/Account/ChangePasswordForm/ChangePasswordForm';
+import BasicModal from '../components/Modal/BasicModal/BasicModal';
 
 export default function account() {
     const [user, setUser] = useState(undefined);
@@ -41,7 +42,7 @@ function Configuration(props: any) {
 
     return (
         <div className='account__configuration'>
-            <div className='title'>Configuration</div>
+            <h2 className='title'>Configuration</h2>
             <div className='data'>
                 <ChangeNameForm user={user} logout={logout} setReloadUser={setReloadUser} />
                 <ChangeEmailForm user={user} logout={logout} setReloadUser={setReloadUser} />
@@ -53,9 +54,29 @@ function Configuration(props: any) {
 }
 
 function Address() {
+
+    const [showModal, setShowModal] = useState(false)
+    const [titleModal, setTitleModal] = useState("")
+    const [formModal, setFormModal] = useState(null)
+
+    const openModal = (title: string) => {
+        setTitleModal(title)
+        setFormModal(<h2>New Address...</h2>)
+        setShowModal(true)
+    }
+
     return (
-        <div>
-            <h1>Directions</h1>
+        <div className='account__addresses'>
+            <h2 className='title'>
+                Addresses
+                <Icon name="plus" link onClick={() => openModal("New Address")}/>   
+            </h2>
+            <div>
+                <p>Address list</p>
+            </div>
+            <BasicModal show={showModal} setShow={setShowModal} title={titleModal}>
+                <p>Form</p>
+            </BasicModal>
         </div>
     )
 }
