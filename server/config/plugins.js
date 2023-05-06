cadbdb2f6f39d6058a7e5c5236096bf29f339e64
Nahuel61920 <estebancarrizo619@gmail.com>
2023-05-06 02:26:05 -0300

    aws

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
         config: {
             provider: "aws-s3",
             providerOptions: {
-                accessKeyId: "AKIAUWNDBFKZUDV7YJKW",
-                secretAccessKey: "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",
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_ACCESS_SECRET,
                 region: "eu-west-1",
                 params: {
                     Bucket: "inspired-gamer-bucket-ecommer",
